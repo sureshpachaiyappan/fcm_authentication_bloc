82dc99851104ce7efc4e34f74fd989c48ccf6d02
pachaiyappan_s <sureshp20493@gmail.com>
2024-08-23 21:30:36 +0530

    files upload

---
+++ lib/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<void> logout() async {
+    final GoogleSignIn googleSign = GoogleSignIn();
+    await googleSign.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Profile page'),
+          ElevatedButton(
+            onPressed: () async {
+              await logout();
+              Navigator.pop(context);
+            },
+            child: const Text('Logout'),
+          ),
+        ],
+      ),
+    ));
+  }
+}
